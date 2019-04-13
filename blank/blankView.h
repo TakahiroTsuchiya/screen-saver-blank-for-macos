9829fd5ad348897fa1aef1f510d19816351e5f94
Takahiro Tsuchiya <sample@sample.com>
2019-04-14 06:11:13 +0900

    Initial Commit

---
+++ blank/blankView.h
@@ -0,0 +1,13 @@
+//
+//  blankView.h
+//  blank
+//
+//  Created by Takahiro Tsuchiya on 2019/04/14.
+//  Copyright © 2019 Takahiro Tsuchiya. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface blankView : ScreenSaverView
+
+@end
